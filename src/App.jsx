6a3a1daa7fb1388d5904fd6a6a0b9793a3264193
ea32460 <ea32460@seeu.edu.mk>
2026-05-27 22:43:39 +0200

    Added wildcard route

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import MoviesPage from './pages/MoviesPage'
+import NotFoundPage from "./pages/NotFoundPage";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        {/* TODO: add a wildcard route that handles unknown paths */}
+      </Routes>
+    </>
+  )
+}
+
+export default App
